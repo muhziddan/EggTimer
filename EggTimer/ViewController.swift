6e4df0c6f1e87952479fefcedca456870e75d1d1
muhammad ziddan hidayatullah <ziddan.mz@gmail.com>
2022-04-24 07:27:18 +0700

    make a timer function, add new label

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,12 +10,8 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var titleLabel: UILabel!
-    
-    //    let eggTime : [String : Int] = [
-    //        "Soft": 300,
-    //        "Medium": 480,
-    //        "Hard": 720
-    //    ]
+    @IBOutlet weak var progressView: UIProgressView!
+    @IBOutlet weak var additionalLabel: UILabel!
     
     let eggTime : [String : Int] = [
         "Soft": 5,
@@ -23,30 +19,43 @@ class ViewController: UIViewController {
         "Hard": 12
     ]
     
+    var timePassed = 0
     var timer = Timer()
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
     
     @IBAction func hardnessSelected(_ sender: UIButton) {
         
         timer.invalidate()
         
+        additionalLabel.alpha = 0.0
+        progressView.progress = 0
+        
         guard let title = sender.titleLabel?.text else { return }
         print(title)
         
-        guard var result = eggTime[title] else {return}
+        guard let result = eggTime[title] else {return}
         print(result)
         
-        timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { (Timer) in
-            if result > 0 {
-                print ("\(result) seconds")
-                result -= 1
+        titleLabel.text = "Cooking your \(title) egg~"
+        
+        UIView.animate(withDuration: 0.5, delay: 0.2, options: .curveEaseIn, animations: {self.progressView.alpha = 1.0}, completion: nil)
+        
+        timeProgress(totalTime: result)
+    }
+    
+    func timeProgress(totalTime: Int) {
+        timePassed = 0
+        self.timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { (Timer) in
+            if self.timePassed < totalTime {
+                self.timePassed += 1
+                print ("\(self.timePassed) seconds, progress \(Float(self.timePassed) / Float(totalTime))")
+                self.progressView.setProgress(Float(self.timePassed) / Float(totalTime), animated: true)
             } else {
                 Timer.invalidate()
                 self.titleLabel.text = "Done!!!"
+                UIView.animate(withDuration: 0.3, delay: 0.5, options: .curveEaseOut, animations: {
+                    self.progressView.alpha = 0.0
+                    self.additionalLabel.alpha = 1.0
+                }, completion: nil)
             }
         }
         
